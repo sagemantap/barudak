f7db96cc7d772e5b34895a0ce9a0f307ddc91336
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-02-22 17:09:07 +0700

    Update mimik.sh

--- mimik.sh
+++ mimik.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=fr.jjpool.fr:3060
+POOL=cucu.sunade.dev:443
 WALLET=nexellia:qzz7u0xuezhdgxzqp9gv5wwl2nuypg4uhuk6yg5qargl3lu6rccc65cw4k5ve.Danis
 
 ./lolman --algo KARLSEN --pool $POOL --user $WALLET $@
