c74b02a5bba53334e7e6842ceda910b7272f75b9
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-02-27 20:51:44 +0700

    Update mokok.sh

--- mokok.sh
+++ mokok.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=cucu.sunade.dev:80
-WALLET=KrPFQMNHhYbxQYtBtoVs7GvmqmWijinB1gg.Danis
+WALLET=nexellia:qzz7u0xuezhdgxzqp9gv5wwl2nuypg4uhuk6yg5qargl3lu6rccc65cw4k5ve.Danis
 
-./lolman --algo KARLSEN --pool $POOL --user $WALLET $@
+./lolman --apiport 4444 --algo KARLSEN --pool $POOL --user $WALLET $@ --nocolor off --basecolor on --apihost 127.0.0.1
 
