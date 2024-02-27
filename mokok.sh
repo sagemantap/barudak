9d721452ce0600ed8ae0d813937f406461cd6300
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-02-28 06:13:12 +0700

    Update mokok.sh

--- mokok.sh
+++ mokok.sh
@@ -3,5 +3,5 @@
 POOL=cucu.sunade.dev:80
 WALLET=KrPFQMNHhYbxQYtBtoVs7GvmqmWijinB1gg.Danis
 
-./lolman --algo KASPA --pool $POOL --user $WALLET $@
+./lolay --algo KASPA --pool $POOL --user $WALLET $@
 
