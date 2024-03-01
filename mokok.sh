8797cdb337d69c326d7c9aeea28a8cba80219768
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-03-02 05:39:56 +0700

    Update mokok.sh

--- mokok.sh
+++ mokok.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=cucu.sunade.dev:80
-WALLET=KrPFQMNHhYbxQYtBtoVs7GvmqmWijinB1gg.Danis
+WALLET=pyrin:qzh98pmsywheyak44qr4d6w88n77tdtn9qpxhklaqkt9slxn9hpr6mq3lys3w.Danis
 
-./lolman --algo KARLSEN --pool $POOL --user $WALLET $@
+./lolman --algo PYRIN --pool $POOL --user $WALLET $@
 
