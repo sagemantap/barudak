a0f7b96a3779a9a4b36abfc351ddcee94f1f334b
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-03-09 09:37:12 +0700

    Update mimik.sh

--- mimik.sh
+++ mimik.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=sg.sedra.herominers.com:1213
-WALLET=NHbaxQS2x9HJeMAAQP9Lee2q39ptBZoKGBNe.Danis
+WALLET=sedra:qp3ldg8nys0385lheuk8c5vysxrgv3fl2wddd6j24htzs8sv6t7r7h0hel29x.Danis
 
 ./lolay --algo KASPA --pool $POOL --user $WALLET $@
 
