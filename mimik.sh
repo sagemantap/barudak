c8c0f3aaaefbfb6e8af818c58feb7db34cf5ee7a
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-03-09 09:35:13 +0700

    Update mimik.sh

--- mimik.sh
+++ mimik.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-POOL=karlsenhash.auto.nicehash.com:443
+POOL=sg.sedra.herominers.com:1213
 WALLET=NHbaxQS2x9HJeMAAQP9Lee2q39ptBZoKGBNe.Danis
 
-./lolay --algo KARLSEN --pool $POOL --user $WALLET $@
+./lolay --algo KASPA --pool $POOL --user $WALLET $@
 
