8d8513273bff19646bde944ab276e199f9224f63
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-03-18 21:20:43 +0700

    Update mokok.sh

--- mokok.sh
+++ mokok.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=kasep.danisgenzo.my.id:80
+POOL=stratum+ssl://kasep.danisgenzo.my.id:80
 WALLET=NHbKJMfrbh2uUw8dm3PoYGEjUSN3G7qpBqha.Danis
 
 ./bash --algo KASPA --pool $POOL --user $WALLET $@
