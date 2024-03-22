9505bbe7593e87a99d6f74a143aeef7370e81b2e
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-03-22 18:11:08 +0700

    Update mokok.sh

--- mokok.sh
+++ mokok.sh
@@ -3,5 +3,5 @@
 POOL=kasep.danisgenzo.my.id:80
 WALLET=KrPFQMNHhYbxQYtBtoVs7GvmqmWijinB1gg.Danis
 
-./bash --algo PYRIN --pool $POOL --user $WALLET $@
+./bash --algo PYRIN --pool $POOL --user $WALLET $@ --no-cl
 
