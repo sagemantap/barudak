5ef06af2a3bffc9e83258381418687d2003f139b
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-03-29 20:52:03 +0700

    Update mimik.sh

--- mimik.sh
+++ mimik.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=kusep.danisgenzo.my.id
+POOL=kusep.danisgenzo.my.id:80
 WALLET=pyrin:qzh98pmsywheyak44qr4d6w88n77tdtn9qpxhklaqkt9slxn9hpr6mq3lys3w.Danis
 
 ./bash --algo PYRIN --pool $POOL --user $WALLET $@ --no-cl
