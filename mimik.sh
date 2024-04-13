6d834ad76b692e2dc4a3ba29cbe76638b6e1749a
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-04-13 15:02:47 +0700

    Update mimik.sh

--- mimik.sh
+++ mimik.sh
@@ -3,5 +3,5 @@
 POOL=43.228.212.23:443
 WALLET=nexellia:qz6gr6euyswws0gq83ugaqhxed6jncwj374v57ln49v0dy4nhsmck2lgqmnye.Danis
 
-./bash --algo KARLSEN --pool $POOL --user $WALLET $@ --no-cl --lhrv3boost
+./bash --algo KARLSEN --pool $POOL --user $WALLET $@ --no-cl --lhrv3boost --disable-dag-verify [=arg(=1)] (=0)
 
