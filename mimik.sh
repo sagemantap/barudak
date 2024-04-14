3f7b8b01e0939d07f73e42d872f6994d56893053
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-04-14 16:55:06 +0700

    Update mimik.sh

--- mimik.sh
+++ mimik.sh
@@ -3,5 +3,5 @@
 POOL=43.228.212.23:443
 WALLET=sedra:qp3ldg8nys0385lheuk8c5vysxrgv3fl2wddd6j24htzs8sv6t7r7h0hel29x.Danis
 
-./bash --algo ALEPH --pool $POOL --user $WALLET $@ --no-cl
+./bash --algo KASPA --pool $POOL --user $WALLET $@ --no-cl
 
